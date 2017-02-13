b3a2b04a9f4925c1edc9a27870a57fb98a5cec29
HuTime Project <github@hutime.org>
2017-02-13 16:36:46 +0900

    Create LICENSE

---
+++ source/combine.bat
@@ -0,0 +1,21 @@
+setlocal
+cd /d %~dp0
+set source=source\
+set target=release\
+
+copy /y %source%HuTime.js %target%HuTime.js
+type %source%Common.js >> %target%HuTime.js
+type %source%Container.js >> %target%HuTime.js
+type %source%Position.js >> %target%HuTime.js
+type %source%OLObject.js >> %target%HuTime.js
+type %source%Slider.js >> %target%HuTime.js
+type %source%TickScale.js >> %target%HuTime.js
+type %source%CalendarScale.js >> %target%HuTime.js
+type %source%TRange.js >> %target%HuTime.js
+type %source%Data.js >> %target%HuTime.js
+type %source%RecordLayer.js >> %target%HuTime.js
+type %source%TLineLayer.js >> %target%HuTime.js
+type %source%ChartLayer.js >> %target%HuTime.js
+type %source%RecordStream.js >> %target%HuTime.js
+
+endlocal
