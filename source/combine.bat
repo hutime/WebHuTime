5d4a83eff89fc2ebbd476f0941b5008b335f6c87
sekinot <sekino@hutime.org>
2017-02-16 11:11:14 +0900

    rename HuTime.js

--- source/combine.bat
+++ source/combine.bat
@@ -3,7 +3,7 @@ cd /d %~dp0
 set source=.\
 set target=..\release\
 
-copy /y %source%HuTime.js %target%HuTime.js
+copy /y %source%HuTimeRoot.js %target%HuTime.js
 type %source%Common.js >> %target%HuTime.js
 type %source%Container.js >> %target%HuTime.js
 type %source%Position.js >> %target%HuTime.js
